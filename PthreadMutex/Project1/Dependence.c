b1a6aac6619acb67737cc1d1c0ee01bf640880ea
TestGoing <3054105120@qq.com>
2018-10-12 15:47:21 +0800

    删除了Dependence.c Dependece.h

--- PthreadMutex/Project1/Dependence.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "Dependence.h"
-
-void dependence_test()
-{
-	printf("Hello World\n");
-}
--- PthreadMutex/Project1/Dependence.h
+++
@@ -1,6 +0,0 @@
-#ifndef DEPENDENCE_H
-#define DEPENDENCE_H
-
-void dependence_test(void);
-
-#endif
